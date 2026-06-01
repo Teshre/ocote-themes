f7fcaaa0bbeddba95cb7b52be6de4a22c2051800
Eduardo Perry Rangel <teshre@zohomail.com>
2026-06-01 13:30:02 -0600

    change themes

---
+++ dist/wezterm/mezcal.lua
@@ -0,0 +1,22 @@
+-- Ocote Themes — Mezcal
+-- Agave y oro. Terroso y dorado.
+-- https://github.com/Teshre/ocote-themes
+-- Usage: require('ocote-themes.wezterm.mezcal')
+
+return {
+  background    = "#13110E",
+  foreground    = "#E0D8C8",
+  cursor_bg     = "#D9A441",
+  cursor_border = "#D9A441",
+  cursor_fg     = "#13110E",
+  selection_bg  = "#36301E",
+  selection_fg  = "#E0D8C8",
+  ansi = {
+    "#2C2818", "#DA6E54", "#A8B84A", "#D9A441",
+    "#8AA6C0", "#C28AA8", "#6FC8AE", "#D2C8B0",
+  },
+  brights = {
+    "#4E462C", "#EC8A6E", "#C0D066", "#ECC468",
+    "#A6BED4", "#DCA6C2", "#8EE0C8", "#F2EAD6",
+  },
+}
---
+++ dist/wezterm/noche.lua
@@ -0,0 +1,22 @@
+-- Ocote Themes — Noche
+-- Azules profundos para la madrugada.
+-- https://github.com/Teshre/ocote-themes
+-- Usage: require('ocote-themes.wezterm.noche')
+
+return {
+  background    = "#0C0E16",
+  foreground    = "#CBD4EC",
+  cursor_bg     = "#82A6E0",
+  cursor_border = "#82A6E0",
+  cursor_fg     = "#0C0E16",
+  selection_bg  = "#232A40",
+  selection_fg  = "#CBD4EC",
+  ansi = {
+    "#20253A", "#E2727E", "#7CC596", "#D8C062",
+    "#7AA0E8", "#B79AE0", "#6DD8D0", "#BAC2DC",
+  },
+  brights = {
+    "#3A4260", "#F08A96", "#98D8AE", "#E8D484",
+    "#9CBAF0", "#CEB4EC", "#8EE6DE", "#E6ECFA",
+  },
+}
